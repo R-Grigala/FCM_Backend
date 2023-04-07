faab78b9ef9bd28a4f5bcc2f1f33f430e8e9b143
R-Grigala <r.grigalashvili777@gmail.com>
2023-04-07 18:17:56 +0400

    Coding Continue:// add Models

--- base/models.py
+++ base/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class FCMToken(models.Model):
+    id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.token
+        return str(self.updated_at)
+    
