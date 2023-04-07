2b6bd1eb36c27c2e71aa2e17b8d81b802047f728
R-Grigala <r.grigalashvili777@gmail.com>
2023-04-07 12:28:26 +0400

    Coding Continue:// Testing FCM notification

--- send_notification.py
+++ send_notification.py
@@ -26,8 +26,33 @@ def send_notification(registration_ids, message_title, message_desc):
     print(result.json())
 
 
+def send_fcm(registration_ids, message_title, message_desc):
+    fcm_api = "AAAA8ILaOzc:APA91bFRapskgwOn3YgPI--eXo63jJalU5rd8FJbDOu3b0O5r1SyqyHnDZnOtPgp4_tf5T50MwGT90s-Y_0ozVW5uuR6Df4D9aF1ucu4jjpjlBihZO-4YI9xMCzDWzHMVQrwq25huxkt"
+    url = "https://fcm.googleapis.com/fcm/send"
+
+    headers = {
+        "Content-Type":"application/json",
+        "Authorization": 'key=' + fcm_api
+    }
+
+    payload = {
+        "registration_ids" : registration_ids,
+        "priority" : "high",
+        "notification" : {
+            "body": message_desc,
+            "title" : message_title,
+            "image" : "",
+            "icon" : ""
+        }
+    }
+
+    result = requests.post(url, data=json.dumps(payload), headers=headers)
+    print(result.json())
+
+
 registratin_ids2 = ['ezT9dNbGRUWRr9j2NrpaPs:APA91bGx2JJZ1o2KbMntRBVAoc5d6X0HEESkuiHYOln3wlfcnxAguMmMIqL_kPCArVoQ-V1Pd4e5a2src3YX0Ppt2nSDNxXdby6FpV6lTosip3f2d68ev-Bv6aomCVbDm9Yl7bDNgbqn']
 registratin_ids = ['d4BY7MyVTyqqNHbUXrJo_E:APA91bF8MNbmeh-onVWzmOBQCtuWXpI-Jbh10tL6y6fi-qu4sIIOev_rbpYdAwIFmEeD5vLPwjCuK2KfXPlrKMNJTW30TUG6dZ_O4iYwsbvvw_PqmMFhRmiUGQlbR6UCcCFd4oqo64m-']
 message_desc = "Test Test"
 message_title = "Test Test"
 send_notification(registration_ids=registratin_ids2, message_title='Test Test', message_desc='Test Test')
+send_fcm(registration_ids=registratin_ids, message_title='Send Notification From send_fcm', message_desc='Test Test')
